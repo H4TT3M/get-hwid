291c5f5c4b7e13b94cfae61f7b4d889ec550bede
pedrokpp <71797980+pedrokpp@users.noreply.github.com>
2020-12-27 20:24:25 -0300

    Initial commit

---
+++ hwid.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import time
+
+banner = r"""
+
+
+      o                                o            o  
+     <|>                             _<|>_         <|> 
+     / >                                           < \ 
+     \o__ __o     o              o     o      o__ __o/ 
+      |     v\   <|>   by: kp   <|>   <|>    /v     |  
+     / \     <\  < >            < >   / \   />     / \ 
+     \o/     o/   \o    o/\o    o/    \o/   \      \o/ 
+      |     <|     v\  /v  v\  /v      |     o      |  
+     / \    / \     <\/>    <\/>      / \    <\__  / \ 
+                                                   
+
+"""
+
+def main():
+    print(banner)
+    if not os.name == "nt":
+        print("This program only works on Windows.")
+        time.sleep(3)
+        exit()
+    hwid = str(subprocess.check_output('wmic csproduct get uuid')).split('\\r\\n')[1].strip('\\r').strip()
+    print(f"Your hwid is {hwid}.")
+    print("Copied to your clipboard ;)")
+    os.system('echo ' + hwid + '| clip')
+    print(' ')
+    os.system("pause")
+
+if __name__ == "__main__":
+    main()
